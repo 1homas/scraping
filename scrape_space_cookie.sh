a817bea2e0900fa155d7a612011e37cf2171c61c
Thomas Howard <1@1homas.org>
2024-06-19 17:25:43 -0700

    Renamed to match the .py script

--- scrape_space_cookie.sh
+++ scrape_space_cookie.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Use this to run a Python script within a virtual environment.
+#
 source /Users/thomas/hack/Projects/scraping/.venv/bin/activate
 /Users/thomas/hack/Projects/scraping/scrape_space_cookie.py
 deactivate
