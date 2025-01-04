122d2ce2f7c6c956893a64209f13014fea9291ae
Thomas Howard <1@1homas.org>
2025-01-04 06:58:07 -0800

    Formatted

--- selenium_ddg.py
+++ selenium_ddg.py
@@ -10,15 +10,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-url = 'https://duckduckgo.com'
-query = 'python web scraping'
+url = "https://duckduckgo.com"
+query = "python web scraping"
 
 driver = webdriver.Firefox()
-driver.set_window_size(1920,1080)
+driver.set_window_size(1920, 1080)
 driver.get(url)
-assert 'DuckDuckGo' in driver.title
+assert "DuckDuckGo" in driver.title
 
-elem = driver.find_element(By.ID, 'searchbox_input')  # Find the search box
+elem = driver.find_element(By.ID, "searchbox_input")  # Find the search box
 elem.send_keys(query + Keys.RETURN)
 
 # browser.quit()
--- selenium_hello.py
+++ selenium_hello.py
@@ -4,17 +4,17 @@ import random
 from selenium import webdriver
 
 urls = [
-  'https://nytimes.com',
-  'https://latimes.com',
-  'https://cisco.com',
+    "https://nytimes.com",
+    "https://latimes.com",
+    "https://cisco.com",
 ]
 # driver = webdriver.Chrome()
 driver = webdriver.Firefox()
 
 driver.get(random.choice(urls))
-driver.implicitly_wait(10) # applies to every element call for the entire session
+driver.implicitly_wait(10)  # applies to every element call for the entire session
 
-# wait = WebDriverWait(driver, timeout=2) 
-# wait.until(lambda d : revealed.is_displayed()) # poll for a specific condition to be true 
+# wait = WebDriverWait(driver, timeout=2)
+# wait.until(lambda d : revealed.is_displayed()) # poll for a specific condition to be true
 
 driver.quit()
